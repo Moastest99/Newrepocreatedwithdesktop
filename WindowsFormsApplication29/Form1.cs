fbdcd290b61753b74c0082748d346ce7b2ad0624
Moastest99 <Moatest99@gmail.com>
2018-10-29 18:36:28 +0100

    Solution a l'issue 1
    Init et Cast du nullable

--- WindowsFormsApplication29/Form1.cs
+++ WindowsFormsApplication29/Form1.cs
@@ -18,8 +18,8 @@ namespace WindowsFormsApplication29
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int? i = null;
-            int j = 512 + i;
+            int? i = 0;
+            int j = 512 + (int)i;
         }
     }
 }
